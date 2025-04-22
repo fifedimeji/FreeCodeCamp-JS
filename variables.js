a18362a75cdfc75dc0e7bf8135bbe997dd517e4e
Fife Oladimeji <fifedimeji@gmail.com>
2025-04-22 17:12:36 +0100

    Initial commit

---
+++ variables.js
@@ -0,0 +1,79 @@
+// VARIABLES
+// how to initiate string data types
+// let favouriteFruit = 'Guava'
+// let favouriteIceCream = "Blackberry Syrup"
+// let favouriteProgrammingLanguage = 'Javascript'
+
+// // how to initiate number data types
+// let numberOfEpisodes = 24
+// let pi = 3.142
+
+// // how to initiate BigInt data types
+// let veryLargeNumber = 536753648826542578398652n
+
+// // how to initiate boolean data types
+// let lovesCodimg = true
+
+// // how to initiate undefined data types
+// let favouriteColour
+
+// // how to initiate null data types
+// favouriteFruit = (null)
+
+// // how to initiate symbol data types
+// const uniqueKey = Symbol()
+
+// // how to initiate object data types
+// // let course = {
+// //     name: 'Javascript for Beginners',
+// //     hours: 3 
+// // }
+
+// // reassigning data types with dynamic typing
+// let firstName = 'Fife'
+// firstName = 50
+// firstName = true
+
+// objects...
+// let course = {
+//     name: 'Javascript for Beginners',
+//     hours: 3 
+// }
+// console.log(course.name)
+
+// course.name = 'Javascript Fundamentals'
+// console.log(course.name)
+
+// console.log(course['name'])
+
+// course['name'] = 'Javascript 101'
+// console.log(course['name'])
+
+// let property = ['hours']
+// console.log(course[property])
+
+// arrays...
+// let productColours = ['blue', 'green']
+
+// productColours[0] = '42'
+
+// console.log(productColours.length)
+
+// functions...
+// function sayHi(name) {
+//     console.log('Hi ' + name)
+// }
+
+// sayHi('Fife')
+
+// function multiply(num1, num2) {
+//     return num1 * num2
+// }
+// console.log(multiply(5, 10))
+
+// operators...
+// arithmetic operators...
+// subtraction -, addition +, multiplication *, division /, modulo % (used to find remainder after division, to determine odd and even numbers)
+
+// comparison operators...
+// 
